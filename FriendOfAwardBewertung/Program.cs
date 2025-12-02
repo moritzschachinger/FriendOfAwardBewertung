ef2a951506711dad06ffce0623387e99ec8fd6c2
lusei1403 <luksei1403@gmail.com>
2025-12-02 12:31:29 +0100

    Amdin Login Page

--- FriendOfAwardBewertung/Program.cs
+++ FriendOfAwardBewertung/Program.cs
@@ -1,4 +1,8 @@
+using FriendOfAwardBewertung;
 using FriendOfAwardBewertung.Components;
+
+// SPAA: für Authorization
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,13 +10,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// SPAA: Service für Authorization
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<MyCustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+  provider.GetRequiredService<MyCustomAuthStateProvider>());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
