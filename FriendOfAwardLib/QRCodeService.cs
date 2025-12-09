f2e8b068284ac7a9265270ca33c9eacf34c0432d
lusei1403 <luksei1403@gmail.com>
2025-12-09 11:57:02 +0100

    QRCodeService

---
+++ FriendOfAwardLib/QRCodeService.cs
@@ -0,0 +1,23 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class QRCodeService
+{
+    public string GenerateToken()
+    {
+        return Guid.NewGuid().ToString("N"); // 32 Zeichen
+    }
+
+    public byte[] GenerateQRCodeImage(string url)
+    {
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+        QRCode qrCode = new QRCode(qrCodeData);
+
+        using Bitmap bitmap = qrCode.GetGraphic(20);
+        using MemoryStream ms = new MemoryStream();
+        bitmap.Save(ms, ImageFormat.Png);
+        return ms.ToArray();
+    }
+}
