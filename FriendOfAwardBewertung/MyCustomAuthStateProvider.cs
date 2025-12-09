7bc7dc9ea0af60b66475925ef97fbac2410f2c9f
lusei1403 <luksei1403@gmail.com>
2025-12-09 11:49:32 +0100

    Rolle Admin

--- FriendOfAwardBewertung/MyCustomAuthStateProvider.cs
+++ FriendOfAwardBewertung/MyCustomAuthStateProvider.cs
@@ -4,33 +4,61 @@ using System.Security.Claims;
 namespace FriendOfAwardBewertung
 {
 
-	public class MyCustomAuthStateProvider : AuthenticationStateProvider
-	{
-		private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
-		// _anonymous - falls _currentUser null ist
-		private ClaimsPrincipal? _currentUser = null;
+    public class MyCustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+        // _anonymous - falls _currentUser null ist
+        private ClaimsPrincipal? _currentUser = null;
 
 
-		public override Task<AuthenticationState> GetAuthenticationStateAsync()
-		{
-			return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
-		}
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
+        }
+        public void LoginAdmin(string email)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, email),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "AdminAuth");
+            _currentUser = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
 
 
-		public void Login(string username)
-		{
-			ClaimsIdentity identity = new([new Claim(ClaimTypes.Name, username)],
-					"MyCustomAuthType");  // von SPAA erfunden, keiner der Standardtypen
+        public void LoginUser(string token)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, token),
+                new Claim(ClaimTypes.Role, "User")
+            };
 
-			_currentUser = new ClaimsPrincipal(identity);
-			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
-		}
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "UserAuth");
+            _currentUser = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
 
 
-		public void Logout()
-		{
-			_currentUser = null;
-			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
-		}
-	}
+        public void Login(string username)
+        {
+            ClaimsIdentity identity = new([new Claim(ClaimTypes.Name, username)],
+                    "MyCustomAuthType");  // von SPAA erfunden, keiner der Standardtypen
+
+            _currentUser = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+
+        public void Logout()
+        {
+            _currentUser = null;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
 }
